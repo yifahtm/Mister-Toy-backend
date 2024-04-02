f085a18c49ae03f01e614ddc20d849b32326325d
Yifaht Meir <yifahtmyers@gmail.com>
2024-04-02 18:27:35 +0300

    Connect atlas

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yifaht:2S2s2s2s2s2s2s@cluster0.crpigkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    // dbURL: 'mongodb://127.0.0.1:27017',
     dbName: 'toy_db',
 }
